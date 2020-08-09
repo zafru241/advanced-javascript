931a11465635f7414e45a49ec957b94ffbb2cb92
zafru241 <syedzafrullah99@gmail.com>
2020-08-10 01:33:26 +0600

    array-split

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [1,2,3,4,5,6,7,8]
+// const part = nums.slice(2,5);
+const removed = nums.splice(2,5,77);
+
+// console.log(removed);
+// console.log(nums);
+
+const joinTogether = nums.join(",");
+console.log(joinTogether);
