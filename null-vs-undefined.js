39e2d08d120d76344b688f8d737c58db12936f30
zafru241 <syedzafrullah99@gmail.com>
2020-08-09 20:53:12 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let pakhi;
+// function add(sum1, sum2){
+//     console.log(sum1 + sum2);
+//     return //...return with variable dile defined or undefined//
+// }
+// function add(num1,num2){
+//     console.log(num1 , num2)
+// }
+// const person = {name:"zafru", address:"Shamoly"}
+// console.log(person.age);
+// let fun = undefined;
+// console.log(fun);
+let age =[7,9,11];
+console.log(age[4]);
+
